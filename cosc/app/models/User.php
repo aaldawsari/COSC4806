229876b12a745969c34e145ffb7e5dc5a3a15f32
aaldawsari <32252151+aaldawsari@users.noreply.github.com>
2017-10-31 04:09:09 -0400

    adding feature
    -adding lock to login if user attempts 3 or more.
    - keep logs of user login, logout in DB.

--- cosc/app/models/User.php
+++ cosc/app/models/User.php
@@ -5,7 +5,8 @@ class User {
     public $username="";
     public $password="";
     public $auth = false;
-
+    public $attempts=0;
+    public $lockedUntil=0;
     public function __construct() {
         
     }
@@ -15,7 +16,13 @@ class User {
          * if username and password good then
          * $this->auth = true;
          */
-		 
+
+
+        if($this->lockedUntil >= time()){
+            //locked
+        }else{
+
+
 		$db = db_connect();
         $statement = $db->prepare("select * from users
                                 WHERE username = :name;
@@ -24,15 +31,39 @@ class User {
         $statement->execute();
         $usrs = $statement->fetch();
 
-        if (isset($usrs[0])) {
+        if(isset($usrs[0])) {
             if (password_verify($this->password, $usrs[1])) {
                 $_SESSION['usr']=$this->username;
                 $_SESSION['pass']=$this->password;
                 $this->auth=true;
+                $_SESSION['attempts']=0;
+                $statement = $db->prepare("INSERT INTO regLogs (date, username, stat, logType)
+                                values(:currdate, :username, :stat, :type);
+                ");
+                $statement->bindValue(':currdate', date('Y-m-d H:i:s'));
+                $statement->bindValue(':username', $this->username);
+                $statement->bindValue(':stat', 1);
+                $statement->bindValue(':type', "login");
+                $statement->execute();
+            }else{
+                $this->attempt();
             }
+        }else{
+            $this->attempt();
         }
+       }
     }
-	
+	public function attempt(){
+
+            if($this->attempts>=3){
+                $this->lockedUntil= (time() + 60);
+                $_SESSION['lockTime'] = $this->lockedUntil;
+            } else{
+                $this->attempts++;
+                $_SESSION['attempts'] = $this->attempts;;
+            }
+
+    }
 	public function register ($username, $password) {
 
         $dbh = db_connect();
--- cosc/app/views/templates/footer.php
+++ cosc/app/views/templates/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div>
-            <p>Abdullah Aldossari</p>
+            <p>Abdullah Aldossari <?php echo $_SESSION['attempts']; echo $_SESSION['lockTime'] ?></p>
 
     </div>
 </footer>
